48334c5a2e1d7c2439176ecade8b2aca09017022
Philipp Dobrigkeit <p.dobrigkeit@goalio.de>
2013-05-28 12:05:56 +0200

    Missing files for Composer installation

---
+++ src/GoalioPasswordManagement/Module.php
@@ -0,0 +1,23 @@
+<?php
+namespace GoalioPasswordManagement;
+
+use Zend\Loader\StandardAutoloader;
+use Zend\Loader\AutoloaderFactory;
+
+class Module {
+
+    public function getAutoloaderConfig() {
+        return array(
+            AutoloaderFactory::STANDARD_AUTOLOADER => array(
+                StandardAutoloader::LOAD_NS => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+}
+
